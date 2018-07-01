9d25862c9c72f790e827aecf73dc8a4b01e12406
KenGitHubCode <mucke001@cougars.csusm.edu>
2018-07-01 11:40:44 -0700

    Basic Functionality Setup

---
+++ app/src/main/java/com/example/android/tourguideapp/CategoryAdapter.java
@@ -0,0 +1,79 @@
+package com.example.android.tourguideapp;
+
+/**
+ * Created by redne on 7/1/2018.
+ */
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    //Number of tabs
+    static final int NUM_ITEMS = 4;
+
+    /**
+     * Context of the app
+     */
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     * @param context is the context of the app
+     * @param fm      is the fragment manager that will keep each fragment's state in the adapter
+     *                across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    /**
+     * Functions called when use swipes or taps tab
+     * Starts new fragments for each
+     * @param position: tab click or page swiped to by user
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new KyotoFragment();
+            case 1:
+                return new OsakaFragment();
+            case 2:
+                return new HiroshimaFragment();
+            case 3:
+                return new TokyoFragment();
+            default:
+                return new KyotoFragment();
+        }
+    }
+
+    //Hardcode number of tabs in the view
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_kyoto);
+            case 1:
+                return mContext.getString(R.string.category_osaka);
+            case 2:
+                return mContext.getString(R.string.category_tokyo);
+            case 3:
+                return mContext.getString(R.string.category_hiroshima);
+            default:
+                return mContext.getString(R.string.category_kyoto);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/tourguideapp/TokyoFragment.java
@@ -0,0 +1,217 @@
+package com.example.android.tourguideapp;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ken Muckey on 7/1/2018.
+ */
+
+public class TokyoFragment extends Fragment {
+
+    /**
+     * global variables
+     */
+    // Medai Player and Audio Manager: instance that will be assigned during onclick listener
+    private MediaPlayer mp;
+    private AudioManager mAudioManager;
+    // Audio Focus and set to default false
+    private boolean mAudioFocusGranted = false;
+    // Context for Audio Focus methods
+    private Context mContext;
+
+
+    //REQUIRED empty public constructor
+    public TokyoFragment() {
+        // REQUIRED empty public constructor
+    }
+
+    /**
+     * Audio Focus Listener / management
+     * To ensure app reacts with other apps' audio appropriately
+     * Code base from : https://medium.com/google-developers/how-to-properly-handle-audio-interruptions-3a13540d18fa
+     */
+    private AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                        // Pause playback because your Audio Focus was
+                        // temporarily stolen, but will be back soon. i.e. for a phone call
+                        if (mp != null && mp.isPlaying() == true) {
+                            mp.pause();
+                        }
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // Stop playback, because you lost the Audio Focus.
+                        // i.e. the user started some other playback app
+                        releaseMediaPlayer();
+                    } else if (focusChange ==
+                            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Lower the volume, because something else is also
+                        // playing audio over you. i.e. for notifications or navigation directions
+                        if (mp != null) {
+                            mp.setVolume(0.1f, 0.1f);
+                        }
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Resume playback and normal volume, because you hold the Audio Focus
+                        // again! i.e. the phone call ended or the nav directions are finished
+                        if (mp != null) {
+                            mp.setVolume(1f, 1f);
+                        }
+                        requestAudioFocus();
+                        mp.start();
+                    }
+                }
+            };
+
+    /**
+     * OnCreateView
+     * Used instead of OnCreate due to fragment requirements
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create and set rootView to inflate the list within the container
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        // Assign the {@link AudioManager} to request audio focus
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        // Set context
+        mContext = getActivity().getApplicationContext();
+
+        // Create ArrayList  , using ArrayList for variable size array
+        final ArrayList<Word> words = new ArrayList<Word>();
+        // Add values to the ArrayList item
+        words.add(new Word("Attraction 1", "Desctiption or Locaiton information", R.drawable.t1));
+        words.add(new Word("Attraction 2", "Desctiption or Locaiton information", R.drawable.k1));
+
+        //initialize itemsAdapter using words ArrayList
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_kyoto);
+        //Initialize listView as the list View from the applicable xml file
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        //set the adapter for listView (which is "list" view in the applicable xml) to itemsView using words
+        listView.setAdapter(adapter);
+
+
+        /**
+         *  Set on item click listener block
+         *  Creates Variable of clicked item, releases media player, requests Audio focus,
+         *      assigned media resource to player and starts
+         * listens for completion of media player and then releases
+         */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Create class instance to reference when assigning resource
+                Word selectedWord = words.get(position);
+
+                // RELEASE media player before assigning new media
+                releaseMediaPlayer();
+
+                //request Audio FOCUS before playing
+                if (requestAudioFocus()) {
+                    //ASSIGN RESOURCE based on position of clicked item and play ie start
+                    mp = MediaPlayer.create(getActivity(), selectedWord.getItemAudio());
+                    //PLAY the mediaplayer
+                    mp.start();
+                    //RELEASE: SetOnCompletionListen to ivoke RELEASE after playback to reduce memory usage
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            releaseMediaPlayer();
+                        }
+                    });
+                }
+
+            }
+        }); // END setOnItemClickListener
+
+        // Inflate the layout for this fragment
+        return rootView;
+    } // End OnCreateView
+
+    /**
+     * Audio Focus request
+     * Code base from : https://gist.github.com/kuccello/5816882
+     */
+    private boolean requestAudioFocus() {
+        if (!mAudioFocusGranted) {
+
+            // Request audio focus for play back
+            int result = mAudioManager.requestAudioFocus(afChangeListener,
+                    // Use the music stream.
+                    AudioManager.STREAM_MUSIC,
+                    // Request permanent focus.
+                    AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                mAudioFocusGranted = true;
+            } else {
+                // FAILED
+                Log.e("Error", ">>>>>>>>>>>>> FAILED TO GET AUDIO FOCUS <<<<<<<<<<<<<<<<<<<<<<<<");
+            }
+        }
+        return mAudioFocusGranted;
+    }
+
+    /**
+     * Audio Focus abandon for when focus is lost
+     * Code base from : https://gist.github.com/kuccello/5816882
+     */
+    private void abandonAudioFocus() {
+        AudioManager am = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        int result = am.abandonAudioFocus(afChangeListener);
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mAudioFocusGranted = false;
+        } else {
+            // FAILED
+            Log.e("Error",
+                    ">>>>>>>>>>>>> FAILED TO ABANDON AUDIO FOCUS <<<<<<<<<<<<<<<<<<<<<<<<");
+        }
+        afChangeListener = null;
+    }
+
+    /**
+     * OnStop now only Cleans up the media player by releasing its resources.
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mp != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mp.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mp = null;
+        }
+
+        // Abandond Focus when Media Player resource is released
+        abandonAudioFocus();
+
+    } // END releaseMediaPlayer
+
+} // END class
+
